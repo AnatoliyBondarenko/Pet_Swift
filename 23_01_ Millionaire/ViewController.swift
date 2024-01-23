4f974320f4790c5f995543932a3766aebc6c3033
AnatoliyBondarenko <bond1304avb@gmail.com>
2024-01-23 20:39:59 +0300

    Initial Commit

---
+++ 23_01_ Millionaire/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  23_01_ Millionaire
+//
+//  Created by mac on 23.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
